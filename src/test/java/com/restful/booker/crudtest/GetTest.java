4fb73ba20c68a5fbf4101365eb1d4a7dbcd50dd2
rutvisha <rutvishapatel9@gmail.com>
2024-01-16 21:46:33 +0000

    rutu

---
+++ src/test/java/com/restful/booker/crudtest/GetTest.java
@@ -0,0 +1,44 @@
+package com.restful.booker.crudtest;
+
+import com.restful.booker.model.BookingPojo;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetTest {
+    @Test
+    public void getAllBookings() {
+
+        BookingPojo bookingPojo = new BookingPojo();
+        bookingPojo.setUsername("admin");
+        bookingPojo.setPassword("password123");
+
+        Response response = given()
+                .body(bookingPojo)
+                .when()
+                .get("/booking");
+        response.then().statusCode(200);
+        response.prettyPrint();
+    }
+
+    @Test
+    public void getBookingById() {
+
+        BookingPojo bookingPojo = new BookingPojo();
+        bookingPojo.setUsername("admin");
+        bookingPojo.setPassword("password123");
+
+        Response response = given()
+                .pathParam("id",1160)
+                .body(bookingPojo)
+                .when()
+                .get("/booking"+"/{id}");
+        response.then().statusCode(200);
+
+        response.prettyPrint();
+    }
+
+
+
+}
